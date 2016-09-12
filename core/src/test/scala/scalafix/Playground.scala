ff71c7f8419f0698aed910db194d2f6f23b983f3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-12 11:17:30 +0100

    WIP

--- core/src/test/scala/scalafix/Playground.scala
+++ core/src/test/scala/scalafix/Playground.scala
@@ -8,8 +8,15 @@ class Playground extends FunSuite {
   import scala.meta._
 
   test("part 1: tokens") {
+    x match {
+      case Fatal(e) =>
+    }
+
     val structure = "val x = 2".tokenize.get.structure
-    logger.elem(structure)
+    q"def foobar = 2" match {
+      case d: Defn.Def =>
+        val x = 2
+    }
   }
 
 }
