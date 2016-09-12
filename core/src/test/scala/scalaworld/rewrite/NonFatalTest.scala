2c4de3b93c39103ed65a3e74d89331cad542509b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-12 13:20:47 +0100

    WIP

---
+++ core/src/test/scala/scalaworld/rewrite/NonFatalTest.scala
@@ -0,0 +1,23 @@
+package scalaworld.rewrite
+
+import scala.meta.inputs.Input
+import scalaworld.util.logger
+
+import org.scalatest.FunSuite
+
+class NonFatalTest extends FunSuite {
+  test("basic") {
+    val obtained = NonFatal.rewrite(
+      Input.String(
+        """|
+           |object a {
+           |  try danger() catch {
+           |    case e: Throwable =>
+           |      println(e)
+           |  }
+           |} """.stripMargin
+      ))
+    logger.elem(obtained)
+  }
+
+}
