ca12531b382a4e0f4337dca59557838dae6971d0
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-12 13:40:57 +0100

    WIP

--- core/src/test/scala/scalaworld/rewrite/NonFatalTest.scala
+++ core/src/test/scala/scalaworld/rewrite/NonFatalTest.scala
@@ -1,27 +1,44 @@
 package scalaworld.rewrite
 
 import scala.meta.inputs.Input
+import scalaworld.Fixed
+import scalaworld.util.DiffAssertions
 import scalaworld.util.logger
 
 import org.scalatest.FunSuite
 
-class NonFatalTest extends FunSuite {
-  test("basic") {
-    val obtained = NonFatal.rewrite(
-      Input.String(
-        """|
-           |object a {
-           |  // comment
-           |  try danger()
-           |
-           |
-           |  catch {
-           |    case e: Throwable =>
-           |      println(e)
-           |  }
-           |} """.stripMargin
-      ))
-    logger.elem(obtained)
+class NonFatalTest extends FunSuite with DiffAssertions {
+
+  def check(original: String, expected: String): Unit = {
+    test(logger.reveal(original)) {
+      val obtained = NonFatal.rewrite(Input.String(original)).get
+      assertNoDiff(obtained, expected)
+    }
   }
+
+  check(
+    """|
+       |object a {
+       |  // comment
+       |  try danger()
+       |
+       |
+       |  catch {
+       |    case e: Throwable =>
+       |      println(e)
+       |  }
+       |} """.stripMargin,
+    """|
+       |object a {
+       |  // comment
+       |  try danger()
+       |
+       |
+       |  catch {
+       |    case NonFatal(e) =>
+       |      println(e)
+       |  }
+       |} """.stripMargin
+  )
 
 }
