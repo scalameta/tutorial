6f1319c14534f69050ed998269ee17f9ef774dfb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-13 16:26:30 +0100

    Experiment with auto-publish

--- bin/testAll.sh
+++ bin/testAll.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-sbt clean coverage test
-sbt "; publishLocal ; scripted ; cli/pack"
-sbt coverageAggregate
-
-# Integration tests
-./bin/nailgun_integration_test.sh
-
+# sbt clean coverage test
+# sbt "; publishLocal ; scripted ; cli/pack"
+# sbt coverageAggregate
+#
+# # Integration tests
+# ./bin/nailgun_integration_test.sh
+#
