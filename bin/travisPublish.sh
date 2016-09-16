7f9fcd45b81d8e288fee20375878be87be9fa05a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-16 11:38:51 +0200

    Disable travis publish

--- bin/travisPublish.sh
+++ bin/travisPublish.sh
@@ -1,6 +1,1 @@
-openssl aes-256-cbc -K $encrypted_3f3a855bcd06_key -iv $encrypted_3f3a855bcd06_iv
-  -in deploy_key.enc -out deploy_key -d
-eval `ssh-agent -s`
-ssh-add deploy_key
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+#!/usr/bin/env bash
