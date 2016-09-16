ae52f2ab5d710ce154adef2d2aa47b2b5ca03207
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-16 11:39:36 +0200

    WIP

--- bin/testAll.sh
+++ bin/testAll.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-# sbt clean coverage test
-# sbt "; publishLocal ; scripted ; cli/pack"
-# sbt coverageAggregate
-#
-# # Integration tests
-# ./bin/nailgun_integration_test.sh
-#
+sbt readme/run
+
