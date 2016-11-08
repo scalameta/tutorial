6a1f2873ab622dba203aa99305040d710c97c5db
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-11-08 19:02:16 +0100

    Implement generic for sealed ADT.

--- macros/src/main/scala/scalaworld/macros/generic.scala
+++ macros/src/main/scala/scalaworld/macros/generic.scala
@@ -3,9 +3,120 @@ package scalaworld.macros
 import scala.collection.immutable.Seq
 import scala.meta._
 
+// Before:
+// @generic
+// case class Foo(i: Int, s: String)
+//
+// @generic
+// sealed trait Bar
+// object Bar {
+//   case class Baz(i: Int)     extends Bar
+//   case class Quux(s: String) extends Bar
+// }
+//
+// After:
+// case class Foo(i: Int, s: String)
+// object Foo {
+//   implicit val FooGeneric: _root_.shapeless.Generic[Foo] =
+//     new _root_.shapeless.Generic[Foo] {
+//       import shapeless._
+//       type Repr = Int :: String :: HNil
+//       def from(r: Repr): Foo = r match {
+//         case i :: s :: HNil => new Foo(i, s)
+//       }
+//       def to(t: Foo): Repr = t.i :: t.s :: HNil
+//     }
+// }
+// sealed trait Bar
+// object Bar {
+//   implicit val BarGeneric: _root_.shapeless.Generic[Bar] =
+//     new _root_.shapeless.Generic[Bar] {
+//       import shapeless._
+//       type Repr = Baz :+: Quux :+: CNil
+//
+//       def from(r: Repr): Bar = r match {
+//         case Inl(t)      => t
+//         case Inr(Inl(t)) => t
+//         case Inr(Inr(_)) => sys.error("This can't happen")
+//       }
+//
+//       def to(t: Bar): Repr = t match {
+//         case t: Baz  => Inl(t)
+//         case t: Quux => Inr(Inl(t))
+//       }
+//     }
+//   case class Baz(i: Int) extends Bar()
+//   case class Quux(s: String) extends Bar()
+// }
+
+object ClassOrTrait {
+  def unapply(any: Any): Option[(Seq[Mod], Type.Name)] = any match {
+    case t: Defn.Class => Some((t.mods, t.name))
+    case t: Defn.Trait => Some((t.mods, t.name))
+    case _             => None
+  }
+}
+
 class generic extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    def createGeneric(name: Type.Name, paramss: Seq[Seq[Term.Param]]): Stat = {
+    def mkCoproductTerm(depth: Int): Term =
+      if (depth <= 0) q"Inl(t)"
+      else q"Inr(${mkCoproductTerm(depth - 1)})"
+
+    def mkCoproductPat(depth: Int): Pat =
+      if (depth <= 0) p"Inl(t)"
+      else p"Inr(${mkCoproductPat(depth - 1)})"
+
+    def mkCantHappen(depth: Int): Pat =
+      if (depth <= 0) p"Inr(_)"
+      else p"Inr(${mkCantHappen(depth - 1)})"
+
+    def mkSealedGeneric(superName: Type.Name,
+                        subTypes: Seq[Defn.Class]): Stat = {
+      val coproductType: Type = subTypes.foldRight[Type](t"CNil") {
+        case (cls, accum) =>
+          Type.ApplyInfix(cls.name, t":+:", accum)
+      }
+      val coproductTermCases: Seq[Case] = subTypes.zipWithIndex.map {
+        case (cls, i) =>
+          p"case t: ${cls.name} => ${mkCoproductTerm(i)}"
+      }
+      val coproductTerm = q"t match { ..case $coproductTermCases }"
+      val coproductPat: Seq[Case] = subTypes.zipWithIndex.map {
+        case (cls, i) =>
+          p"case ${mkCoproductPat(i)} => t"
+      }
+      val cantHappen =
+        p"""case ${mkCantHappen(subTypes.length - 1)} =>
+              sys.error("This can't happen")
+         """
+      mkGeneric(superName,
+                coproductType,
+                coproductTerm,
+                coproductPat :+ cantHappen)
+    }
+
+    def mkGeneric(name: Type.Name,
+                  repr: Type,
+                  to: Term,
+                  from: Seq[Case]): Stat = {
+      val reprTyp: Stat = q"type Repr = $repr"
+      val toDef: Stat   = q"def to(t: $name): Repr = $to"
+      val fromDef: Stat =
+        q"def from(r: Repr): $name = r match { ..case $from }"
+      val implicitName = Term.Name(name.syntax + "Generic")
+      q"""implicit val ${Pat.Var
+        .Term(implicitName)}: _root_.shapeless.Generic[$name] =
+            new _root_.shapeless.Generic[$name] {
+              import shapeless._
+              $reprTyp
+              $fromDef
+              $toDef
+            }
+       """
+    }
+
+    def mkClassGeneric(name: Type.Name, paramss: Seq[Seq[Term.Param]]): Stat = {
       val params = paramss match {
         case params :: Nil => params
         case _             => abort("Can't create generic for curried functions yet.")
@@ -17,7 +128,10 @@ class generic extends scala.annotation.StaticAnnotation {
       }
       val hlistTerm: Term = params.foldRight[Term](q"HNil") {
         case (param, accum) =>
-          Term.ApplyInfix(q"t.${Term.Name(param.name.value)}", q"::", Nil, Seq(accum))
+          Term.ApplyInfix(q"t.${Term.Name(param.name.value)}",
+                          q"::",
+                          Nil,
+                          Seq(accum))
       }
       val hlistPat: Pat = params.foldRight[Pat](q"HNil") {
         case (param, accum) =>
@@ -25,45 +139,53 @@ class generic extends scala.annotation.StaticAnnotation {
                            q"::",
                            Seq(accum))
       }
-      val args       = params.map(param => Term.Name(param.name.value))
-      val repr: Stat = q"type Repr = $hlistType"
-      val to: Stat   = q"def to(t: $name): Repr = $hlistTerm"
+      val args = params.map(param => Term.Name(param.name.value))
       val patmat =
         Case(hlistPat, None, q"new ${Ctor.Ref.Name(name.value)}(..$args)")
-      val from: Stat =
-        q"""def from(r: Repr): $name = r match {
-              ..case ${Seq(patmat)}
-            }
-         """
-      q"""implicit val ${Pat.Var.Term(Term.Name(name.syntax + "Generic"))} =
-            new _root_.shapeless.Generic[$name] {
-              import shapeless._
-              $repr
-              $from
-              $to
-            }
-       """
+      mkGeneric(name, hlistType, hlistTerm, Seq(patmat))
     }
+
+    def isSealed(mods: Seq[Mod]): Boolean = mods.exists(_.syntax == "sealed")
+
     defn match {
+      case Term.Block(
+          Seq(t @ ClassOrTrait(mods, name), companion: Defn.Object))
+          if isSealed(mods) =>
+        val oldTemplStats = companion.templ.stats.getOrElse(Nil)
+        val subTypes = oldTemplStats.collect {
+          case t: Defn.Class => t
+        }
+        val newStats = mkSealedGeneric(name, subTypes) +: oldTemplStats
+        val newCompanion =
+          companion.copy(templ = companion.templ.copy(stats = Some(newStats)))
+        println(newCompanion)
+        Term.Block(Seq(t, newCompanion))
       // companion object exists
       case Term.Block(
           Seq(cls @ Defn.Class(_, name, _, ctor, _),
               companion: Defn.Object)) =>
         val newStats =
-          createGeneric(name, ctor.paramss) +:
+          mkClassGeneric(name, ctor.paramss) +:
             companion.templ.stats.getOrElse(Nil)
         val newCompanion =
           companion.copy(templ = companion.templ.copy(stats = Some(newStats)))
+        println(newCompanion.syntax)
         Term.Block(Seq(cls, newCompanion))
       // companion object does not exists
       case cls @ Defn.Class(_, name, _, ctor, _) =>
         val companion =
-          q"object ${Term.Name(name.value)} { ${createGeneric(name, ctor.paramss)} } "
+          q"object ${Term.Name(name.value)} { ${mkClassGeneric(name, ctor.paramss)} } "
         println(companion.syntax)
         Term.Block(Seq(cls, companion))
+      case cls @ Defn.Class(_, name, _, ctor, _) =>
+        val companion =
+          q"object ${Term.Name(name.value)} { ${mkClassGeneric(name, ctor.paramss)} } "
+        println(companion.syntax)
+        Term.Block(Seq(cls, companion))
+      // Sealed ADT
       case _ =>
         println(defn.structure)
-        abort("@WithApply must annotate a class.")
+        abort("@generic must annotate a class or a sealed trait/class.")
     }
   }
 }
--- macros/src/test/scala/scalaworld/macros/GenericExample.scala
+++ macros/src/test/scala/scalaworld/macros/GenericExample.scala
@@ -3,10 +3,15 @@ package scalaworld.macros
 @generic
 case class Foo(i: Int, s: String)
 
-object GenericExample {
-  def main(args: Array[String]): Unit = {
-    val f     = Foo(1, "string")
-    val hlist = implicitly[shapeless.Generic[Foo]].to(f)
-    println(hlist)
-  }
+@generic
+sealed trait Bar
+object Bar {
+  case class Baz(i: Int)     extends Bar
+  case class Quux(s: String) extends Bar
+}
+
+object GenericExample extends App {
+  println(implicitly[shapeless.Generic[Foo]].to(Foo(1, "string")))   // 1 :: "string" :: HNil
+  println(implicitly[shapeless.Generic[Bar]].to(Bar.Baz(1)))         // Inl(Baz(1))
+  println(implicitly[shapeless.Generic[Bar]].to(Bar.Quux("string"))) // Inr(Inl(Quux(string)))
 }
