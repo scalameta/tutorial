9a0459f224d845d54087750754452b27ef8dfc8b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-16 16:08:49 +0200

    known_hosts

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 mkdir -p $HOME/.ssh
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 DEPLOY_KEY_FILE=$HOME/.ssh/id_rsa
 echo "$GITHUB_DEPLOY_KEY" > ${DEPLOY_KEY_FILE}
 chmod 600 ${DEPLOY_KEY_FILE}
