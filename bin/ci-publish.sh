6554c2e28138de055334f6682706187fb155921e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-16 13:05:55 +0200

    Add ssh key globally

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -3,4 +3,6 @@ set -eu
 DEPLOY_KEY_FILE=$HOME/github_rsa
 echo "$GITHUB_DEPLOY_KEY" > ${DEPLOY_KEY_FILE}
 chmod 600 ${DEPLOY_KEY_FILE}
-ssh-agent bash -c "ssh-add $DEPLOY_KEY_FILE; sbt -Dsbt.ivy.home=/drone/.ivy2/ readme/publish"
+eval "$(ssh-agent -s)"
+ssh-add ${DEPLOY_KEY_FILE}
+sbt -Dsbt.ivy.home=/drone/.ivy2/ readme/publish
