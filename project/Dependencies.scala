88f0a1cb63086c98e8f8d12838e157c94bc0d709
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-22 10:19:08 +0200

    Add notice about scalamacros in section on macro annotations.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 import sbt._, Keys._
 object Dependencies {
   lazy val MetaVersion     = "1.8.0"
-  lazy val ParadiseVersion = "3.0.0-M9"
+  lazy val ParadiseVersion = "3.0.0-M10"
   lazy val scala211        = "2.11.11"
-  lazy val scala212        = "2.12.2"
+  lazy val scala212        = "2.12.3"
   lazy val scalameta       = "org.scalameta" %% "scalameta" % MetaVersion
   lazy val contrib         = "org.scalameta" %% "contrib" % MetaVersion
   lazy val testkit         = "org.scalameta" %% "testkit" % MetaVersion
