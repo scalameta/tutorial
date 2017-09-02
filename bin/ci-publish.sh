e24608117b85b052da8d2a8f7a1efc03e44aa296
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-02 16:09:40 +0200

    Enable CI on Travis

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -eu
-mkdir -p $HOME/.ssh
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-git config --global user.email "olafurpg@gmail.com"
-git config --global user.name "Drone CI bot"
-git config --global push.default simple
-DEPLOY_KEY_FILE=$HOME/.ssh/id_rsa
-echo "$GITHUB_DEPLOY_KEY" > ${DEPLOY_KEY_FILE}
-chmod 600 ${DEPLOY_KEY_FILE}
-eval "$(ssh-agent -s)"
-ssh-add ${DEPLOY_KEY_FILE}
-sbt -Dsbt.ivy.home=/drone/.ivy2/ readme/publish
+PUBLISH=${CI_PUBLISH:-false}
+SECURE_VAR=${TRAVIS_SECURE_ENV_VARS:-false}
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" == true && -n "$TRAVIS_TAG" && "$CI_PUBLISH" == true ]]; then
+  set -eu
+  mkdir -p $HOME/.ssh
+  ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+  git config --global user.email "olafurpg@gmail.com"
+  git config --global user.name "Drone CI bot"
+  git config --global push.default simple
+  DEPLOY_KEY_FILE=$HOME/.ssh/id_rsa
+  echo "$GITHUB_DEPLOY_KEY" > ${DEPLOY_KEY_FILE}
+  chmod 600 ${DEPLOY_KEY_FILE}
+  eval "$(ssh-agent -s)"
+  ssh-add ${DEPLOY_KEY_FILE}
+  sbt -Dsbt.ivy.home=/drone/.ivy2/ readme/publish
+else
+  echo "Skipping publish..."
+fi
