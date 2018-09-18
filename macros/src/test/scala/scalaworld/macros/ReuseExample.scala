78d31f26be79e36f92d47c8c46590a6fd0f17eb7
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-18 20:57:23 +0200

    Remove the old site

--- macros/src/test/scala/scalaworld/macros/ReuseExample.scala
+++
@@ -1,4 +0,0 @@
-package scalaworld.macros
-
-@Reuse
-class ReuseExample
--- macros/src/test/scala/scalaworld/macros/WithApplyExample.scala
+++
@@ -1,8 +0,0 @@
-package scalaworld.macros
-
-@WithApply
-class WithApplyExample(a: Int)(b: String)
-
-object WithApplyExampleMain {
-  println(WithApplyExample(2)("string"))
-}
